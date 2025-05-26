0df68330fac5b008dd3e7cfe80974c0d4bf1cc86
pal-divyansh <paldivyansh25@gmail.com>
2025-05-27 03:36:20 +0530

    package.json update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -84,6 +84,12 @@ export default {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      transitionDelay: {
+        DEFAULT: '0ms', // Keep a default if needed
+        '[2s]': '2s', // Explicitly define the arbitrary value
+        // Allow arbitrary values for transition delays
+        ...
+      },
     },
   },
   plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
